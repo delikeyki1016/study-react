957da7d3f5261719cfa679842c0ae004d1b9f9fa
delikeyki1016 <delikeyki1016@gmail.com>
2023-12-28 20:46:29 +0900

    오토포커스 내용 추가

--- ch4/todolist-app-class/src/components/InputTodo.jsx
+++ ch4/todolist-app-class/src/components/InputTodo.jsx
@@ -1,7 +1,9 @@
-import {Component} from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
 class InputTodo extends Component {
+    iptRef = React.createRef()
+
     state = {
         todo: ""
     }
@@ -21,6 +23,16 @@ class InputTodo extends Component {
         this.setState({todo: e.target.value})
     }
 
+    // 돔트리에 마운트가 완료되었을 때 최초 한번 실행. input에 포커스 주기
+    componentDidMount = () =>{
+        this.iptRef.current.focus()
+    }
+
+    // DOM 업데이트가 일어난 직후 실행
+    componentDidUpdate(prevProps, prevState) {
+        this.iptRef.current.focus()
+    }
+
     render() {
         console.log('랜더: InputTodo...')
         return (
@@ -28,7 +40,8 @@ class InputTodo extends Component {
                 <div className='col'>
                     <div className='input-group'>
                         <input id="msg" name='msg' type="text" className='form-control' placeholder='할일을 입력하세요' 
-                        value={this.state.todo} onChange={this.changeTodo} onKeyUp={this.enterInput} />
+                        value={this.state.todo} onChange={this.changeTodo} onKeyUp={this.enterInput}
+                        ref={this.iptRef} />
                         <span className='btn btn-primary input-group-addon' onClick={this.addHandler}>추가</span>
                     </div>
                 </div>
